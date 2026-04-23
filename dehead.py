7581c034fcad4976d90fe2ea58862fe77022829f
degD <den.ege.der@gmail.com>
2026-04-23 14:38:29 +0300

    create process_vid

--- dehead.py
+++ dehead.py
@@ -2,6 +2,10 @@
 from pathlib import Path
 from ultralytics import YOLO
 from PIL import Image, ImageDraw, ImageFilter
+import cv2
+import os
+import shutil
+import random
 
 PROJECT_DIR = Path(__file__).parent
 MODEL_PATH = PROJECT_DIR / "best.pt"
@@ -31,12 +35,15 @@ class Dehead:
                 input_path.parent / (input_path.name.split(".")[0] + "-dehead" + input_path.suffix) for input_path in self.input_paths
             ]
 
-    def process_img(self, input_index: int):
+    def process_img(self, input_path: Path, output_path: Path = None):
         model = YOLO(MODEL_PATH)
         model.conf = self.threshold
-        input_path = self.input_paths[input_index]
-        output_path = self.output_paths[input_index]
-        
+        if output_path is None:
+            try:
+                output_path = self.output_paths[self.input_paths.index(input_path)]
+            except ValueError:
+                raise ValueError("Input path not found in the list of input paths. Please provide a valid output path.")
+
         with Image.open(input_path) as img:
             results = model(input_path)
             masks =  results[0].boxes.xywh.tolist()
@@ -67,24 +74,59 @@ class Dehead:
 
             img.save(output_path)
 
-    def process_video(self, input_index: int):
-        # Placeholder for video processing logic
-        pass
+    def process_video(self, input_path: Path, output_path: Path = None):
+        vidcap = cv2.VideoCapture(str(input_path))
+        model = YOLO(MODEL_PATH)
+        model.conf = self.threshold
+        if output_path is None:
+            try:
+                output_path = self.output_paths[self.input_paths.index(input_path)]
+            except ValueError:
+                raise ValueError("Input path not found in the list of input paths. Please provide a valid output path.")
+            
+        frames_input_path = output_path.parent / f"{random.randint(0, 100000)}-frames"
+        os.mkdir(frames_input_path)
+
+        success, image = vidcap.read()
+        count = 0
+        while success:
+            cv2.imwrite(str(frames_input_path / f"frame{count}.jpg"), image)
+            success, image = vidcap.read()
+            count += 1
+
+        frames_output_path = output_path.parent / f"{random.randint(0, 100000)}-frames-dehead"
+        os.mkdir(frames_output_path)
+        for i in range(count):
+            self.process_img(frames_input_path / f"frame{i}.jpg", frames_output_path / f"frame{i}.jpg")
+        shutil.rmtree(frames_input_path)
+
+        frame = cv2.imread(str(frames_output_path / "frame0.jpg"))
+        height, width, _ = frame.shape
+        output_video =  cv2.VideoWriter(str(output_path), 0, 1, (width, height))
+
+        for i in range(count):
+            frame = cv2.imread(str(frames_output_path / f"frame{i}.jpg"))
+            output_video.write(frame)
+            os.remove(frames_output_path / f"frame{i}.jpg")
+        shutil.rmtree(frames_output_path)
+
+        cv2.destroyAllWindows()
+        output_video.release()
 
     def process(self):
         if self.input_paths[0].suffix.lower() in [".jpg", ".jpeg", ".png"]:
             for i in range(len(self.input_paths)):
-                self.process_img(i)
+                self.process_img(self.input_paths[i])
         elif self.input_paths[0].suffix.lower() in [".mp4", ".avi", ".mov"]:
             for i in range(len(self.input_paths)):
-                self.process_video(i)
+                self.process_video(self.input_paths[i])
         else:
             raise ValueError("Unsupported file format. Supported formats are: .jpg, .jpeg, .png, .mp4, .avi, .mov")
 
 if __name__ == "__main__":
     Dehead(
-        input_paths=[PROJECT_DIR / "demo/family.png"],
-        output_paths=[PROJECT_DIR / "demo/family-dehead.png"],
+        input_paths=[PROJECT_DIR / "demo/vid.mp4"],
+        output_paths=[PROJECT_DIR / "demo/family-dehead.mp4"],
         threshold=0.2,
         mask_scale=0.8,
         boxes=False,
